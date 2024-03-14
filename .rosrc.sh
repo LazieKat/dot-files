5ab6a1330575e9b78d014274656531a4e4159815
ahmad <aziad1998@gmail.com>
2024-03-14 14:10:56 -0400

    updated stuff

---
+++ .rosrc.sh
@@ -0,0 +1,5 @@
+export ROS_LOCALHOST_ONLY=1
+export ROS_DOMAIN_ID=30
+source /opt/ros/humble/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash 
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
