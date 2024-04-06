cbb04e6102155dc5ab19685de254ef28b6b6f7db
LazieKat <aziad1998@gmail.com>
2024-04-05 21:46:19 -0400

    updated stuff

--- .rosrc.sh
+++ .rosrc.sh
@@ -5,7 +5,7 @@ source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
 source /usr/share/colcon_cd/function/colcon_cd.sh
 export _colcon_cd_root=/opt/ros/humble/
 
-export MY_ROBOT_IP=192.168.0.90
 
+export MY_ROBOT_IP=192.168.0.90
 export ROS_MASTER_URI=http://$MY_ROBOT_IP:11311
 export ROS_IP=$(hostname -I)
